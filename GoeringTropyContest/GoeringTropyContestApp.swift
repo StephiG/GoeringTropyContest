835bd304f00caae7a71e9658e922a965b9de6520
Stephi <stephigoering@me.com>
2022-10-05 14:08:31 +0200

    Initial Commit

---
+++ GoeringTropyContest/GoeringTropyContestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoeringTropyContestApp.swift
+//  GoeringTropyContest
+//
+//  Created by Stephi Goering on 05.10.22.
+//
+
+import SwiftUI
+
+@main
+struct GoeringTropyContestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
